6144d3108c6759c1b0dab928b772ab573df52177
Newton Migosi <newtonmigosi@gmail.com>
2021-02-07 14:11:19 +0300

    ⚡️ optimize line function using lazy list

--- app/Lines.hs
+++ app/Lines.hs
@@ -2,17 +2,15 @@ module Lines where
 
 data Point = Point {x::Double, y::Double}
 
-linethrough (Point {x0,y0}) (Point {x1,y1}) x = 
-    let a = (y1-y0) / (x1-x0)
-        b = y0 - a * x0
-    in a * x + b
+to = ($)
 
-and = ($)
+linefrom (Point {x0,y0}) (Point {x1,y1}) = 
+    let line    = (round x0, round y0) : map f line
+        f (x,y) = (x+1, y+a)
+        a       = (y1-y0) / (x1-x0)
+    in takeWhile (<= round x1 . fst) line
 
-drawline p0@(Point {x0,y0}) p1@(Point {x1,y1}) color = 
-    let xs = [round x0..round x1]
-        ys = map f xs
-        f  = linethrough p0 `and` p1
-        points = zip xs ys
-    in map (putpixel color) points
+drawline p0 p1 color = 
+    let line = linefrom p0 `to` p1
+    in mapM (putpixel color) line
 
