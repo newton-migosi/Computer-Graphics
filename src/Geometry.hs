7675bf21ae69d888cbd80e0dd75d3896e2a8e40f
Newton Migosi <newtonmigosi@gmail.com>
2021-02-09 21:51:10 +0300

    🚧 added support for homogenous points

--- src/Geometry.hs
+++ src/Geometry.hs
@@ -1,19 +1,21 @@
 module Geometry where
 
+import Prelude hiding (length)
+
 type Vector = [Double]
 
 length v = sqrt (v `dot` v)
 
-scale c v = map (*c) v
+c `scale` v = map (*c) v
 
 normalize v@[0,0,0] = v
 normalize v = (1 / length v) `scale` v
 
 v `dot` w = sum $ zipWith (*) v w
 
-v `projected_onto` w = (v `dot` normalize w) `scale` normalize w
+v `projectedOnto` w = (v `dot` normalize w) `scale` normalize w
 
-v `cos_angle_between` w = normalize v `dot` normalize w
+v `cosAngleBetween` w = normalize v `dot` normalize w
 
 v `orthogonal` w = (v `dot` w)  == 0
 
@@ -26,6 +28,22 @@ v `minus` w = zipWith (-) v w
 type Matrix = [[Double]]
 
 rows = id
-cols m = [map (!!c) m | (c,_)<-zip [0..] (m!!0)]
+cols m = [map (!!c) m | (c,_)<-zip [0..] (head m)]
 
 a `multiply` b = [[c `dot` r | c<- cols b] | r<-rows a]
+
+m `transform` v = map head $ m `multiply` map (:[]) v
+
+epsilon n = replicate n 0 ++ [1] ++ repeat 0
+
+-- homogenous points
+
+mkPoint a b c = [a,b,c,1]
+
+m `transformP` p = 
+    let p' = m `transform` p
+    in case last p' of
+        0  -> p'
+        w' -> (1/w') `scale` p'
+
+mkVec a b c = [a,b,c,0]                  
